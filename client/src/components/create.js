9f1aba685602c71268cce9fe02801aa3927bdd51
meetAlexLau <alex.lau.1@stonybrook.edu>
2021-10-01 18:33:52 -0400

    Create Component UI

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import FontPicker from 'font-picker-react';
 
 export default class Create extends Component {
     constructor(props){
         super(props);
 
+        //https://reactjs.org/docs/handling-events.html
+        // Javascript methods are not bound by default, we have to explicitly bind them to 'this'
         this.onChangeText = this.onChangeText.bind(this)
+        this.onChangeColor = this.onChangeColor.bind(this)
+        this.onChangeFont = this.onChangeFont.bind(this)
+        this.onChangeFontSize = this.onChangeFontSize.bind(this)
+        this.onSubmit = this.onSubmit.bind(this); //submits form
         this.state = {
             renderText: "",
             renderColor: "",
-            renderFont: "",
+            renderFont: "Open Sans",
             renderFontSize: ""
         }
     }
@@ -26,28 +33,55 @@ export default class Create extends Component {
         this.setState({renderColor: e.target.value})
     }
 
+    onChangeFont(e){
+        this.setState({renderFont: e.family})
+    }
 
+    onChangeFontSize(e){
+        this.setState({renderFontSize: e.target.value})
+    }
+
+    onSubmit(e) {
+        e.preventDefault() //prevents default behavior
+    
+        console.log(`Object successfully created!`);
+        console.log(`Name: ${this.state.renderText}`);
+        console.log(`Color: ${this.state.renderColor}`);
+        console.log(`Font: ${this.state.renderFont}`);
+        console.log(`Font Size: ${this.state.renderFontSize}`);
+
+        this.setState({renderText: '', renderColor: '', renderFont: this.state.renderFont, renderFontSize: ''})
+    }
 
     render() {
-      return (<div class="form-wrapper">
-        <Form>
-          <Form.Group controlId="Name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text"/>
+      return (<div className="form-wrapper">
+        <Form onSubmit = {this.onSubmit}>
+          <Form.Group controlId="Text">
+            <Form.Label>Text</Form.Label>
+            <Form.Control type="text" value = {this.state.renderText} onChange = {this.onChangeText}/>
           </Form.Group>
   
-          <Form.Group controlId="Email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email"/>
+          <Form.Group controlId="Color">
+            <Form.Label>Color</Form.Label>
+            <Form.Control type="color" defaultValue = {'#ffffff'} onChange = {this.onChangeColor}/>
           </Form.Group>
   
-          <Form.Group controlId="Name">
-            <Form.Label>Roll No</Form.Label>
-            <Form.Control type="text"/>
+          <Form.Group controlId="Font">
+              <FontPicker
+                apiKey = 'AIzaSyDpvJ4vwYQ5IrLsANU2rngs1ahHODCH4Hw'
+                activeFontFamily = {this.state.renderFont}
+                onChange = {this.onChangeFont} //add className = 'apply-font' to elements you want font to apply to
+              />
+            <Form.Label className = 'apply-font'>Font</Form.Label>
+          </Form.Group>
+
+          <Form.Group controlId="Font Size">
+            <Form.Label>Font Size</Form.Label>
+            <Form.Control type="number" onChange = {this.onChangeFontSize}/>
           </Form.Group>
   
           <Button variant="danger" size="lg" block="block" type="submit">
-            Create Student
+            Create Object
           </Button>
         </Form>
       </div>);
